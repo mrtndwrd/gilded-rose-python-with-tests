8fd6939e61ac913ca3abf7dcdbb9b01e03222908
Maarten de Waard <me@maartendewaard.nl>
2024-08-28 08:59:42 +0000

    first try

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,49 +1,55 @@
+INCREASING_QUALITY_ITEMS = ["Aged Brie",
+                            "Backstage passes to a TAFKAL80ETC concert"]
+CONSTANT_QUALITY_ITEMS = ["Sulfuras, Hand of Ragnaros"]
+
+
 class GildedRose:
     @staticmethod
     def update_quality(items):
         for i in range(0, len(items)):
-            if "Aged Brie" != items[i].name and "Backstage passes to a TAFKAL80ETC concert" != items[i].name:
-                # TODO: Improve this code.  Word.
-                if items[i].quality > 0:
-                    if "Sulfuras, Hand of Ragnaros" != items[i].name:
-                        items[i].quality = items[i].quality - 1
-            else:
-                if items[i].quality < 50:
-                    items[i].quality = items[i].quality + 1
-                    if "Aged Brie" == items[i].name:
-                        if items[i].sell_in < 6:
-                            items[i].quality = items[i].quality + 1
-                    # Increases the Quality of the stinky cheese if it's 11 days to due date.
-                    if "Aged Brie" == items[i].name:
-                        if items[i].sell_in < 11:
-                            items[i].quality = items[i].quality + 1
-                    if "Backstage passes to a TAFKAL80ETC concert" == items[i].name:
-                        if items[i].sell_in < 11:
-                            # See revision number 2394 on SVN.
-                            if items[i].quality < 50:
-                                items[i].quality = items[i].quality + 1
-                        # Increases the Quality of Backstage Passes if the Quality is 6 or less.
-                        if items[i].sell_in < 6:
-                            if items[i].quality < 50:
-                                items[i].quality = items[i].quality + 1
-            if "Sulfuras, Hand of Ragnaros" != items[i].name:
-                items[i].sell_in = items[i].sell_in - 1
-            if items[i].sell_in < 0:
-                if "Aged Brie" != items[i].name:
-                    if "Backstage passes to a TAFKAL80ETC concert" != items[i].name:
-                        if items[i].quality > 0:
-                            if "Sulfuras, Hand of Ragnaros" != items[i].name:
-                                items[i].quality = items[i].quality - 1
-                    else:
-                        # TODO: Fix this.
-                        items[i].quality = items[i].quality - items[i].quality
-                else:
-                    if items[i].quality < 50:
-                        items[i].quality = items[i].quality + 1
-                    if "Aged Brie" == items[i].name and items[i].sell_in <= 0:
-                        items[i].quality = 0
-                        # of for.
-            if "Sulfuras, Hand of Ragnaros" != items[i].name:
-                if items[i].quality > 50:
-                    items[i].quality = 50
+            GildedRose.update_quality_of_item(items[i])
         return items
+
+    @staticmethod
+    def update_quality_of_item(item):
+        GildedRose.update_quality_for_item(item)
+        GildedRose.update_sell_in_for_item(item)
+        if item.sell_in < 0:
+            if item.name in INCREASING_QUALITY_ITEMS:
+                item.quality = 0
+            else:
+                GildedRose.decrease_quality(item)
+
+    @staticmethod
+    def update_quality_for_item(item):
+        if item.name in INCREASING_QUALITY_ITEMS:
+            GildedRose.update_quality_for_increasing_quality_items(item)
+        elif item.name not in CONSTANT_QUALITY_ITEMS:
+            GildedRose.update_quality_for_decreasing_quality_items(item)
+
+    @staticmethod
+    def update_sell_in_for_item(item):
+        if item.name not in CONSTANT_QUALITY_ITEMS:
+            item.sell_in = item.sell_in - 1
+
+    @staticmethod
+    def decrease_quality(item):
+        if item.quality > 0:
+            item.quality = item.quality - 1
+
+    @staticmethod
+    def increase_quality(item):
+        if item.quality < 50:
+            item.quality = item.quality + 1
+
+    @staticmethod
+    def update_quality_for_decreasing_quality_items(item):
+        GildedRose.decrease_quality(item)
+
+    @staticmethod
+    def update_quality_for_increasing_quality_items(item):
+        GildedRose.increase_quality(item)
+        if item.sell_in < 6:
+            GildedRose.increase_quality(item)
+        if item.sell_in < 11:
+            GildedRose.increase_quality(item)
